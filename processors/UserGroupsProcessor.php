e3f883901ef3701b7cb446831878eb082b068af2
Guilherme Godoy <guilhermehlgodoy22@gmail.com>
2026-04-08 10:33:14 +0100

    feat: extract UserGroupsProcessor into shared library

---
+++ processors/UserGroupsProcessor.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file plugins/importexport/csv/shared/processors/UserGroupsProcessor.php
+ *
+ * Copyright (c) 2026 Simon Fraser University
+ * Copyright (c) 2026 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class UserGroupsProcessor
+ *
+ * @ingroup plugins_importexport_csv
+ *
+ * @brief Process the user groups data into the database.
+ */
+
+namespace APP\plugins\importexport\csv\shared\processors;
+
+use APP\facades\Repo;
+use APP\plugins\importexport\csv\classes\cachedAttributes\CachedEntities;
+
+class UserGroupsProcessor
+{
+    public static function process(array $roles, int $userId, int $contextId, string $locale)
+    {
+        foreach ($roles as $role) {
+            $userGroup = CachedEntities::getCachedUserGroupByName($role, $contextId, $locale);
+            if ($userGroup) {
+                Repo::userGroup()->assignUserToGroup($userId, $userGroup->id);
+            }
+        }
+    }
+}
